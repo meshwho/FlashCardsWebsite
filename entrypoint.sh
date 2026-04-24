27838fa9a634b0e021e710771577968d8009ee0a
meshwho <azaaaazzzazaazsazaa9@gmail.com>
2026-04-24 14:00:28 +0300

    Entrypoint edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
+echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
 
-while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
 
